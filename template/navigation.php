f878bba039f18828d8af6ecd3616161835c10e64
Frosty <andrewgphillips74@gmail.com>
2017-11-27 13:32:33 -0500

    new site 2018

--- template/navigation.php
+++ template/navigation.php
@@ -20,22 +20,6 @@
 	</ul>
 	<div id="menu"><img src="<?php echo SITEPATH; ?>/assets/img/menu.png" style="width:100%;"></div>
 	<div id='icon'><img class="rotate" src="<?php echo SITEPATH; ?>/assets/img/burgerload.png" height="79" /></div>
-	
-	<subnav>
-		<div id="openLegend">- View Legend -</div>
-		<div class="subnavInner">
-		<?php
-			echo "<div class='legend'><img src='".SITEPATH."/assets/img/rating.svg' width='20' /> RATING</div>";
-			echo "<div class='legend'><img src='".SITEPATH."/assets/img/veggie.svg' width='20' /> VEGGIE</div>";
-			echo "<div class='legend'><img src='".SITEPATH."/assets/img/spicy.svg' width='20' /> SPICY</div>";
-			echo "<div class='legend'><img src='".SITEPATH."/assets/img/seasonal.svg' width='20' /> SEASONAL</div>";
-			echo "<div class='legend'><img src='".SITEPATH."/assets/img/available.svg' width='20' /> EXTINCT</div>";
-			echo "<div class='legend'><img src='".SITEPATH."/assets/img/hasChallenge.svg' width='20' /> FOOD CHALLENGE</div>";
-			echo "<div class='legend'><img src='".SITEPATH."/assets/img/fusion.svg' width='20' /> FUSION</div>";
-			echo "<div class='legend'><img src='".SITEPATH."/assets/img/hasMods.svg' width='20' /> MODDED</div>";
-		?>
-		</div>
-	</subnav>
 </nav>
 
 <div id='shade'></div>
